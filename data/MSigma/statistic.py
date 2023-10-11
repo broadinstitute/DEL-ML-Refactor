8320b4278688524292da3384cfe33197d8b72ec7
chengkua <chengkua@gpbd5-975.broadinstitute.org>
2023-10-11 16:06:46 -0400

    take log10 scale of the plot

--- data/MSigma/statistic.py
+++ data/MSigma/statistic.py
@@ -45,11 +45,11 @@ if __name__ == "__main__":
     df_CK1a_orthosteric = copy.deepcopy(df_CK1a_all_labels[df_CK1a_all_labels['customlabel'] == 'Orthosteric'])
     df_CK1a_allosteric = copy.deepcopy(df_CK1a_all_labels[df_CK1a_all_labels['customlabel'] == 'Allosteric'])
     df_CK1a_cryptic = copy.deepcopy(df_CK1a_all_labels[df_CK1a_all_labels['customlabel'] == 'Cryptic'])
-    df_CK1a_orthosteric['ZScore'] = df_CK1a_orthosteric['ZScore_A']
-    df_CK1a_allosteric['ZScore'] = np.mean(df_CK1a_allosteric[['ZScore_A', 'ZScore_A_inh']], axis=1)
-    df_CK1a_cryptic['ZScore'] = df_CK1a_cryptic['ZScore_A_inh']
+    df_CK1a_orthosteric['log 10 ZScore'] = np.log10(df_CK1a_orthosteric['ZScore_A'])
+    df_CK1a_allosteric['log 10 ZScore'] = np.log10(np.mean(df_CK1a_allosteric[['ZScore_A', 'ZScore_A_inh']], axis=1))
+    df_CK1a_cryptic['log 10 ZScore'] = np.log10(df_CK1a_cryptic['ZScore_A_inh'])
     df_effectsize_boxenplot = pd.concat([df_CK1a_orthosteric, df_CK1a_allosteric, df_CK1a_cryptic])
-    sns.boxenplot(data=df_effectsize_boxenplot.sort_values('customlabel'), x='customlabel', y='ZScore', palette=palette)    
+    sns.boxenplot(data=df_effectsize_boxenplot.sort_values('customlabel'), x='customlabel', y='log 10 ZScore', palette=palette)    
     plt.savefig(os.path.join(config['output_path'], 'lib_stat', 'CK1a', 'ZScore.png'), bbox_inches='tight')
     plt.clf()
 
@@ -57,10 +57,10 @@ if __name__ == "__main__":
     df_CK1d_orthosteric = copy.deepcopy(df_CK1d_all_labels[df_CK1d_all_labels['customlabel'] == 'Orthosteric'])
     df_CK1d_allosteric = copy.deepcopy(df_CK1d_all_labels[df_CK1d_all_labels['customlabel'] == 'Allosteric'])
     df_CK1d_cryptic = copy.deepcopy(df_CK1d_all_labels[df_CK1d_all_labels['customlabel'] == 'Cryptic'])
-    df_CK1d_orthosteric['ZScore'] = df_CK1d_orthosteric['ZScore_D']
-    df_CK1d_allosteric['ZScore'] = np.mean(df_CK1d_allosteric[['ZScore_D', 'ZScore_D_inh']], axis=1)
-    df_CK1d_cryptic['ZScore'] = df_CK1d_cryptic['ZScore_D_inh']
+    df_CK1d_orthosteric['log 10 ZScore'] = np.log10(df_CK1d_orthosteric['ZScore_D'])
+    df_CK1d_allosteric['log 10 ZScore'] = np.log10(np.mean(df_CK1d_allosteric[['ZScore_D', 'ZScore_D_inh']], axis=1))
+    df_CK1d_cryptic['log 10 ZScore'] = np.log10(df_CK1d_cryptic['ZScore_D_inh'])
     df_effectsize_boxenplot = pd.concat([df_CK1d_orthosteric, df_CK1d_allosteric, df_CK1d_cryptic])
-    sns.boxenplot(data=df_effectsize_boxenplot.sort_values('customlabel'), x='customlabel', y='ZScore', palette=palette)    
+    sns.boxenplot(data=df_effectsize_boxenplot.sort_values('customlabel'), x='customlabel', y='log 10 ZScore', palette=palette)    
     plt.savefig(os.path.join(config['output_path'], 'lib_stat', 'CK1d', 'ZScore.png'), bbox_inches='tight')
     plt.clf()
