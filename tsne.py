fc0598481bb9ccd2b13cb29250369a5cfc95f693
chengkua <chengkua@gpbd5-975.broadinstitute.org>
2023-12-05 16:25:53 -0500

    decouple original tsne code from internal dataset

--- tsne.py
+++ tsne.py
@@ -10,32 +10,25 @@ np.random.seed(0)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input_folder", type=str, required=True, help="folder to store features to be reduced")
+    parser.add_argument("--input_file", type=str, required=True, help="folder to store features to be reduced")
+    parser.add_argument("--save_path", type=str, required=True, help="folder to store features to be reduced")
     parser.add_argument("--experiment", type=str, required=True, help="experiment name")
+    parser.add_argument("--perplexity", type=int, default=30, help="perplexity of TSNE")
+    parser.add_argument("--n_jobs", type=int, default=-1, help="number of CPU to run TSNE")
     args = parser.parse_args()
 
-    data_len = []
     data = []
     print("Loading data...")
-    for file in glob.glob(os.path.join(args.input_folder, "*.h5")):
-        if args.experiment in file or 'broad_cp_140k' in file or 'negative' in file:
-            print(file)
-            file_name = os.path.basename(file).replace(".h5", "")
-            lib_name = os.path.basename(os.path.dirname(file))
-            key = "_".join([lib_name, file_name])
-            with h5py.File(file, "r") as f:
-                feature = f["feature"][:]
-            data.append(feature)
-            data_len.append((key, feature.shape[0]))
-    data = np.concatenate(data, axis=0)
+    with h5py.File(args.input_file, "r") as f:
+        feature = f["feature"][:]
+
     print("Running TSNE...")
     t_start = time.time()
-    data_embedded = TSNE(n_components=2, n_jobs=-1, random_state=0).fit_transform(data)
+    data_embedded = TSNE(n_components=2, n_jobs=args.n_jobs, 
+                         perplexity=args.perplexity, random_state=0).fit_transform(feature)
     print('Time elapsed: {} seconds'.format(time.time()-t_start))
 
     print("Saving data...")
-    with open(f"data_embedded_{args.experiment}.pkl", "wb") as f:
+    with open(os.path.join(args.save_path, f"data_embedded_{args.experiment}.pkl"), "wb") as f:
         pickle.dump(data_embedded, f)
-    with open(f"data_embedded_meta_{args.experiment}.pkl", "wb") as f:
-        pickle.dump(data_len, f)
 
